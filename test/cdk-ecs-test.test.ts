21345096a8e521b1a8bf7e16f6e6e36442b23afa
Daniel Fyhr <danielfyhr@gmail.com>
2022-10-04 15:48:17 +0200

    Initial commit

---
+++ test/cdk-ecs-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkEcsTest from '../lib/cdk-ecs-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-ecs-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkEcsTest.CdkEcsTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
